4311f38521a1c66bc3f2ce5e79c160fca95057eb
Vaibhav Bansal <vaibhavbansal23@gmail.com>
2015-08-10 20:12:44 +0530

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,15 @@
+	
+<html>
+	<head>
+	</head>
+	<body>
+	<h1>Welcome Admin</h1>
+		
+		<div class="form">
+			<form action="admin_send.php" method="POST">
+				Enter password :<input type="password" name="pwd">
+				<input type="submit" name="submit" value="submit">
+			</form>		
+		</div>
+	</body> 	
+</html>
